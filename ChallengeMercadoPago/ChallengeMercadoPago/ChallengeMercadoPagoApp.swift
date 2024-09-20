cf3d437432b8d5c41651744a24925ab9ab1bd51d
matiasPalmieri <86312764+matiasPalmieri@users.noreply.github.com>
2024-09-20 17:23:23 -0300

    create new empty project

---
+++ ChallengeMercadoPago/ChallengeMercadoPago/ChallengeMercadoPagoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeMercadoPagoApp.swift
+//  ChallengeMercadoPago
+//
+//  Created by Matias Palmieri on 20/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeMercadoPagoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChallengeMercadoPago/ChallengeMercadoPago/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChallengeMercadoPago
+//
+//  Created by Matias Palmieri on 20/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
